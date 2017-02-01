0e66d210406432beaec9ef0c7dc040b6985b0f13
josgirrui <dragojola@gmail.com>
2017-02-01 16:31:57 +0100

    Test
    Tests

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -1,23 +1,25 @@
 package tests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class Test {
 	
-	@org.junit.Test
+	@Test
 	public void test_1(){
 		assertEquals(5,4+1);
-		assertEquals(5,sum(4,1));
-		assertEquals(5,FuncionSuma.sum(4,1));
+		assertEquals(5,sum(4+1);
+		assertEquals(5,FuncionSuma.sum(4+1);
 		
 
 	}
 	
-	@org.junit.Test
+	@Test
 	public void test_2(){
 		assertEquals(4,4+0);
-		assertEquals(4,sum(4,0));
-		assertEquals(4,FuncionSuma.sum(4,0));
+		assertEquals(4,sum(4+0);
+		assertEquals(4,FuncionSuma.sum(4+0);
 		
 
 	}
